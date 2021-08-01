b4ff7056d1d645756d4fa6cf30dd72b55e6d0f25
Aarathy <aarathym96@gmail.com>
2021-08-02 02:07:00 +0530

    prod firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDaBP78m9vatXxWrStyxfgOB4qL_ilxMPU",
+  authDomain: "prod-8da38.firebaseapp.com",
+  databaseURL: "https://prod-8da38-default-rtdb.firebaseio.com",
+  projectId: "prod-8da38",
+  storageBucket: "prod-8da38.appspot.com",
+  messagingSenderId: "221038562078",
+  appId: "1:221038562078:web:0a7e4297205796f665570f",
+  measurementId: "G-X1NTPKWQ31",
 };
 
 firebase.initializeApp(firebaseConfig);
